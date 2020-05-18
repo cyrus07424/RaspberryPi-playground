6e456537dc47e66e3370ed8352ad8071a8ebc1ef
cyrus <j07424-development@yahoo.co.jp>
2020-05-19 06:44:15 +0900

    Update scripts

--- temperatureToAmbient/temperatureToAmbient.py
+++ temperatureToAmbient/temperatureToAmbient.py
@@ -1,5 +1,6 @@
 import ambient
 import thermal_zone
+import psutil
 import requests
 import time
 
@@ -13,18 +14,31 @@ am = ambient.Ambient(ambientChannelId, ambientWriteKey)
 # Upload to Ambient in a loop forever
 while True:
     try:
-        # Get temperature
+        # Ambient data
+        ambientData = {}
+
+        # Get temperatures
         zone_paths = thermal_zone.get_thermal_zone_paths()
         zone_names = thermal_zone.get_thermal_zone_names(zone_paths)
         zone_temps = thermal_zone.get_thermal_zone_temps(zone_paths)
         # Convert to celsius
         zone_temps = [t / 1000.0 for t in zone_temps]
 
-        # Upload data to Ambient
-        ambientData = {}
+        # Set temperatures to Ambient data
         for i, zone_name in enumerate(zone_names):
             if zone_name == 'cpu-thermal':
                 ambientData['d1'] = zone_temps[i]
+
+        # Get CPU usage
+        cpu_percent = psutil.cpu_percent(interval = 1)
+        ambientData['d2'] = cpu_percent
+
+        # Get memory usage
+        memory = psutil.virtual_memory()
+        ambientData['d3'] = memory.percent
+
+        # Upload data to Ambient
+        print(ambientData)
         r = am.send(ambientData)
     except requests.exceptions.RequestException as e:
         print(e)
